3ee941f90e4d07ec18defbe183c3ec8a17ee35cd
nanophate <nanophate@me.com>
2016-04-24 01:47:29 +0900

    Fix code to remove clutter

--- app.rb
+++ app.rb
@@ -16,63 +16,38 @@ class SinatraApp < Sinatra::Base
     provider :tumblr, ENV['TUMBLR_CONSUMER_KEY'], ENV['TUMBLR_CONSUMER_SECRET']
   end
 
-get '/' do
-  if session[:authenticated]
-    erb :index
-  else
-  	erb :signin
+  get '/' do
+    if session[:authenticated]
+      erb :index
+    else
+      erb :signin
+    end
   end
-end
+	  
+	get '/auth/:provider/callback' do
+	  session[:authenticated] = true
+	  
+	  auth = request.env["omniauth.auth"]
+	  session[:user_id] = auth["uid"]
+	  session[:access_token] = auth['credentials']['token']
+	  session[:access_token_secret] = auth['credentials']['secret']
+	  
+	  Tumblr.configure do |config|
+		config.consumer_key = ENV['TUMBLR_CONSUMER_KEY']
+		config.consumer_secret = ENV['TUMBLR_CONSUMER_SECRET']
+		config.oauth_token = session[:access_token]
+		config.oauth_token_secret = session[:access_token_secret]
+	  end
+	  @@client = Tumblr::Client.new
+	  redirect '/'
+	end
 
-get '/auth/:provider/callback' do
-  session[:authenticated] = true
-  
-  auth = request.env["omniauth.auth"]
-  session[:user_id] = auth["uid"]
-  session[:access_token] = auth['credentials']['token']
-  session[:access_token_secret] = auth['credentials']['secret']
-  
-  Tumblr.configure do |config|
-	config.consumer_key = ENV['TUMBLR_CONSUMER_KEY']
-	config.consumer_secret = ENV['TUMBLR_CONSUMER_SECRET']
-	config.oauth_token = session[:access_token]
-	config.oauth_token_secret = session[:access_token_secret]
-  end
-  @@client = Tumblr::Client.new
-  redirect '/'
-end
-
-post '/' do
-  begin
-    @@client.text("nanophate.tumblr.com",{:title => 'test', :body => 'Life is just a mere dream'})
-  end
-end
-
-get '/auth/failure' do
-  "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
-end
-
-get '/auth/:provider/deauthorized' do
-  "#{params[:provider]} has deauthorized this app."
-end
-
-get '/protected' do
-  throw(:halt, [401, "Not authorized\n"]) unless session[:authenticated]
-  "<pre>#{request.env['omniauth.auth'].to_json}</pre><hr>
-    <a href='/logout'>Logout</a>"
-end
-
-get '/logout' do
-  session[:authenticated] = false
-  redirect '/'
-end
-
-not_found do
-  status 404
-  "not found"
-end
+	post '/' do
+	  begin
+	    @@client.text("nanophate.tumblr.com",{:title => 'test', :body => 'Life is just a mere dream'})
+	  end
+	end
 
 end
-
 
 SinatraApp.run! if __FILE__ == $0
