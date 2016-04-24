cce9e40b58c5caeeabc0b72590e5cde1730c4b3d
nanophate <nanophate@me.com>
2016-04-24 22:35:41 +0900

    Add code to make body and title string

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 require 'tumblr_client'
-#require 'pry'
+require 'pry'
 
   Tumblr.configure do |config|
     config.consumer_key = ENV['TUMBLR_CONSUMER_KEY']
@@ -10,45 +10,5 @@ require 'tumblr_client'
 
   client = Tumblr::Client.new 
 
-  puts client.info['user']['name'].to_s
-
-=begin
-  [1] pry(main)> client.info
-=> {"user"=>
-  {"name"=>"nanophate",
-   "likes"=>4,
-   "following"=>5,
-   "default_post_format"=>"html",
-   "blogs"=>
-    [{"title"=>"Digital Fab & Design",
-      "name"=>"nanophate",
-      "total_posts"=>10,
-      "posts"=>10,
-      "url"=>"http://nanophate.tumblr.com/",
-      "updated"=>146-------,
-      "description"=>"Place where I randomly talk about my idea. Topics from Digital Fabriacation (Tech) to Work Flow management (Design).",
-      "is_nsfw"=>false,
-      "ask"=>false,
-      "ask_page_title"=>"Ask me anything",
-      "ask_anon"=>false,
-      "followed"=>false,
-      "can_send_fan_mail"=>true,
-      "is_blocked_from_primary"=>false,
-      "share_likes"=>true,
-      "likes"=>4,
-      "twitter_enabled"=>true,
-      "twitter_send"=>false,
-      "facebook_opengraph_enabled"=>"N",
-      "tweet"=>"Y",
-      "facebook"=>"N",
-      "followers"=>2,
-      "primary"=>true,
-      "admin"=>true,
-      "messages"=>0,
-      "queue"=>0,
-      "drafts"=>0,
-      "type"=>"public",
-      "reply_conditions"=>2,
-      "subscribed"=>false,
-      "can_subscribe"=>false}]}} 		
-=end
+  puts client.info['user']['name']
+  binding.pry
